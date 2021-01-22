5a57776f2964f91e88ea3bb649e26cfc17d97961
Justyna Bodurka <bodurkajustyna@gmail.com>
2021-01-22 07:31:25 +0100

    header class change,js alert change

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const firstName = 'justyna';
 const age = 36;
-alert ('siema');
+alert ('Witaj drogi odwiedzający');
 console.log(firstName);
 console.log(age);
 console.log(`Witaj drogi odbiorco,nazywam się ${firstName} i mam ${age} lat`);
